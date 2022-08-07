fa34b7b73664b972c567a67857ebf6d412731d9c
Vilhelm <vilhelm.bergqvist@gmail.com>
2022-08-07 21:29:41 +0200

    Put a bunch of new features and bugg fix

--- Uterum.py
+++ Uterum.py
@@ -4,54 +4,32 @@ import Thermometer
 import GPIO
 
 #GPIO skit till RPi
-GPIO.cleanupp()
-GPIO.setmode(GPIO.BCM)
+
 
 #Sätter läget på GPIO pint till att räkna pin numer som BCM, Vilket man behöver googla upp.
 
 #De variablar som vi kommer att behöva för att räkna ut temperaturerna och fläktarnas värde
-FanAuto = True
-FanOn = True
-Utetemp = 21
-Innetemp = 23
 Toltemp = 20, 22
 Agrev = 25
 Dif = 20 
 #Dif är skilnaden i temperatur mellan inne och ute som behövs för att fläktarna ska gå hälften så långsamt
 
-#Sätt in Pin-number för de olika aplikationerna nedan!
-FanPin = 21
-UtePin = 420
-InePin = 70
-OnoffPin = 22
-TruefalsePin = 20
+FanAuto = True
+FanOn = True
 
-#Temporära grejer som ska bytas ut med riktiga prompts:
-ButtonPress = False
-ButtonPress2 = False
-
-def TurnOnAuto():
-        T2.start()
-
-
-
-
-#Detta är en funktion som faktiskt kontrollerar fläktarna
-#Den krävar att man faktiskt  sätter upp de olika GPIO pinsen och PWM på denna pin!
 def Fan(hastighet):
         if hastighet > 100:
-                print (100)
-        elif  hastighet < 20:
-                print (20)
+                GPIO.PWM(100) #Kommer ej ihåg om det här är det faktiskt skriptet.
+        elif  hastighet > 20:
+                GPIO.PWM(20)
         else:
-                print (hastighet)
+                GPIO.PWM(hastighet)
+        #Funktion för att sätta fläkthastigheten
 
-#kör denna som en funktion av multithreading så att den alltid är på.
-#Denna funktionen har hand om alla knapparna som man kan sätta till, kräver att man byter ut ButtonPress till faktiska prompts.
-#Kan lägga till LEDs för att indikera vad den är på.
 def Buttons():
+        global FanAuto, FanOn
         while True:
-                if ButtonPress == True:
+                if GPIO.Button == True:
                         if FanAuto == True:
                                 FanAuto = False
                                 if FanOn == True:
@@ -60,53 +38,49 @@ def Buttons():
                                         Fan(0)
                         elif FanAuto == False:
                                 FanAuto == True
-                        while ButtonPress == True:
-                                time.sleep(0.3)
+                                StartAuto()
+                        while GPIO.Button == True:
+                                time.sleep(0.5)
 
-                if ButtonPress2 == True and FanAuto == False:
+                if GPIO.Button2 == True and FanAuto == False:
                         if FanOn == False:
                                 FanOn = True
                                 Fan(100)
                         if FanOn == True:
                                 FanOn = True
                                 Fan(0)
-                        while ButtonPress2 == True:
-                                time.sleep(0.3)
-                time.sleep(0.5)
-
-#Detta är det huvudsakliga skriptet som ser till att fläktarna kör och att de gör det i rätt hastighet
-
+                        while GPIO.Button2 == True:
+                                time.sleep(0.5)
+                time.sleep(1)
 
 
 def FanAutoSys():
         while True:
-                if FanAuto == True and not(Toltemp[0] <= Innetemp <= Toltemp[1]) and ((Innetemp > Utetemp and Innetemp > Toltemp[1]) or (Innetemp > Utetemp and Innetemp < Toltemp[0])):
-                        UteTemp = Thermometer.read_temp()
-                        IneTemp = Thermometer.read_temp2()
-                        Fan(round(Agrev*abs(Innetemp-(sum(Toltemp)/2))/(abs(Innetemp-Utetemp)*(1/Dif+1))))
+                print("This is also on")
+                UteTemp = Thermometer.read_temp()
+                IneTemp = Thermometer.read_temp2()
+                if not(Toltemp[0] <= IneTemp <= Toltemp[1]) and ((IneTemp > UteTemp and IneTemp > Toltemp[1]) or (IneTemp > UteTemp and IneTemp < Toltemp[0])):
+                        Fan(round(Agrev*abs(IneTemp-(sum(Toltemp)/2))/(abs(IneTemp-UteTemp)*(1/Dif+1))))
                         time.sleep(1)
-
-                elif FanAuto == False:
-                        Break
                 else:
                         time.sleep(1)
+                if FanAuto == False:
+                        pass
+                        #break
+                
 
-
-
-# Denna del är till för ett eventuellt fungerande system där man ska se till att knapparna inte är intryckta.
-def CheckFan(what):
-        if what == "Auto":
-                return(FanAuto)
-        elif what == "On":
-                return (FanOn)
-        else:
-                return('Must be "Auto" or "On"')
- 
 
 #Denna del är det som faktiskt kommer att göra någonting:
 #öppnar en thread på de olika funktionerna
+def StartAuto():
+        T2 = threading.Thread(target=FanAutoSys)
+        T2.start()
+        T2.join()
 
-T1 = threading.Thread(target=Buttons)
-T2 = threading.Thread(target=FanAutoSys)
 
-T1.start()
+if __name__ == "__main__":
+        GPIO.cleanupp()
+        GPIO.setmode(GPIO.BCM)
+        T1 = threading.Thread(target=Buttons)
+        T1.start()
+        StartAuto()
