8f3a864d3e5a59ca5d89348d0280428920bbd939
Vancia100 <110672748+Vancia100@users.noreply.github.com>
2022-09-24 01:14:45 +0200

    Fixed bugs and made it work
    This thing actually works decently now: Has some minor bugs wich will need to be fixed in the future.

--- TestFiles/TestJson.py
+++ TestFiles/TestJson.py
@@ -1,3 +1,4 @@
+
 import json
 
 def FileRaw(Value = None):
@@ -9,30 +10,52 @@ def FileRaw(Value = None):
 
 
 def main():
-    Inp = input("Skirv in värde:")
+    Inp1 = input("Skirv in sökvärde: ")
     Jim = FileRaw()
-    if not(Inp):
-        for item in Jim:
-            try:
-                for thing in item:
-                    print(item,":",item[thing])
-            except TypeError:
-                print("this on error")
-                print(item,":", Jim[item])
+    if not(Inp1):
+        try:
+            for item in Jim:
+                try:
+                    for thing in Jim[item]:
+                        print(item,":", thing,":", Jim[item][thing])
+                except TypeError:
+                    print(item,":", Jim[item])
+        except TypeError:
+            print(Jim)
     else:
         try:
-            for thing in Jim[Inp]:
-                print(thing, Jim[Inp][thing])
-            InputNew = input("skriv in om du vill lägga till yterligare ett sökvärde: ")
-            if InputNew:
-                Jim[Inp] = {InputNew:input(f"Skriv in det nya värdet för värdet {InputNew}: ")}
+            for thing in Jim[Inp1]:
+                print(thing,":", Jim[Inp1][thing])
+            Inp2 = input("skriv in om du vill lägga till yterligare ett sökvärde: ")
+            if Inp2:
+                Jim[Inp1][Inp2] = input(f"Skriv om du vill lägga till ett värde för {Inp2}: ")
+                FileRaw(Jim)
         except KeyError:
-            val = input(f"Nytt sökvärde skriv skriv nytt sökvärde för {Inp}: ")
-            if val:
-                Jim[Inp] = {val:input(f"Skriv värdet för {val}: ")}
+            Inp2 = input(f"Nytt sökvärde skriv skriv nytt sökvärde för {Inp1}: ")
+            if Inp2:
+                Inp3 = input(f"Skriv värdet för {Inp2}: ")
+                if Inp3:
+                    Jim[Inp1] = {Inp2:Inp3}
+                else:
+                    Jim[Inp1] = Inp2
                 FileRaw(Jim)
         except TypeError:
-            print(Jim[Inp])
+            print(Inp1, ":", Jim[Inp1])
+            Inp2 = input(f"Skriv värdet du vill byta ut {Jim[Inp1]} mot: ")
+            if Inp2:
+                Inp3 = input("Skriv in värdet om det ska bli en diktionär: ")
+                if Inp3:
+                    Jim[Inp1] = {Inp2:Inp3}
+                else:
+                    Jim[Inp1] = Inp2
+                FileRaw(Jim)
+
+def definer(Text):
+    try:
+        return(int(Text))
+    except (TypeError):
+        return(bool(Text))
+
 
 if __name__ == "__main__":
     while True:
