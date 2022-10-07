182df8f50ccbd798d387780b630dab29056c9a6a
Vancia100 <vilhelm.bergqvist@gmail.com>
2022-10-08 00:16:04 +0200

    Update TestJsonClass.py

--- TestFiles/TestJsonClass.py
+++ TestFiles/TestJsonClass.py
@@ -14,13 +14,57 @@ def FileRaw(Value = None):
         else:
             return(json.loads(fake.read()))
 
-def MakeDic(Thing):
-    Dic = Thing.__dict__
-    NewDic = {}
-    for i in Dic:
-        if Dic[i] != None:
-            NewDic[i] = Dic[i]
-    return(NewDic)
-            
-Squats = Jimjson(100,90,80,70)
-print(MakeDic(Squats))
+def LoadJson():
+    ImportedList = FileRaw()
+    Jim = {}
+    for i in ImportedList:
+        try:
+            Jim[i] = Jimjson(ImportedList[i]["Pr"], ImportedList[i]["Rep5"], ImportedList[i]["Rep8"], ImportedList[i]["Rep12"])
+        except TypeError:
+            print("imported JSON has errors! \n Pease fix issues and try again.")
+            return(None)
+    return(Jim)
+
+def PrintDic(klass, name=None):
+    try:
+        if name !=None:
+            print(f"{name}:")
+        for i in klass:
+            print(i,":",klass[i])
+    except TypeError:
+        print(klass)
+
+def makeJson(Inp):
+    Jim = {}
+    for i in Inp:
+        Jim[i] = Inp[i].__dict__
+    FileRaw(Jim)
+
+def main():
+    Jim = LoadJson()
+    InpType = input("What if: ")
+    if InpType:
+        try:
+            PrintDic(Jim[InpType].__dict__, InpType)
+        
+            if input("Do you want to change any of the values (blank for no): "):
+                print("Balls")
+        except KeyError:
+            if input("Do you want to add a new value (leave empty for no): " ):
+                Inps = []
+                talks = ["Pr","Rep5","Rep8","Rep12"]
+                for i in range(4):
+                    inps = input(f"How mush weight can you take for {talks[i]} reps?: ")
+                    if not(inps):
+                        inps = None
+                    Inps.append(inps)
+                print(Inps)
+                Jim[InpType] = Jimjson(Inps[0],Inps[1],Inps[2],Inps[3])
+                makeJson(Jim)
+                
+    else:
+        for i in Jim:
+            PrintDic(Jim[i].__dict__, i)
+
+if __name__=="__main__":         
+    main()
